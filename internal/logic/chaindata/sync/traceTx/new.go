df948f82e82260cdaefa9bd709931d8d2296643f
frank <no>
2025-05-20 14:50:19 +0800

    tidy

--- internal/logic/chaindata/sync/traceTx/new.go
+++ internal/logic/chaindata/sync/traceTx/new.go
@@ -41,7 +41,7 @@ func NewTraceSyncer(ctx context.Context, chainId int64, url string, ctxTimeOut t
 	switch chainId {
 	// case 9527, 2025:
 	// 	return newRpgTracer(ctx, chainId)
-	case 1, 11155111, 97, 56, 5000, 5003:
+	case 1, 11155111, 97, 56, 5000, 5003, 84532, 8453:
 		return newMantleTracer(ctx, chainId, url, ctxTimeOut)
 	// case 5000, 5003:
 	// 	return newMantleTracer(ctx, chainId, url, ctxTimeOut)
